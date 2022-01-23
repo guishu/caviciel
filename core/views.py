7ff3b9acedd5592de3cce078cde2f87f6ead0291
guishu <guillaume@schub.fr>
2022-01-23 16:52:54 +0100

    Producer api can only be used by authenticated users

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from core.models import Producer
 from core.serializers import ProducerSerializer, ProducerListSerializer
 
 
 class ProducerViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Producer.objects.all()
 
     def get_serializer_class(self):
