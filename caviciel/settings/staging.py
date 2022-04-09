b75941b0e9a36c0e07b8acc3b8a80eb23c04c127
guishu <guillaume@schub.fr>
2022-04-09 23:39:02 +0200

    make settings/staging usable

--- caviciel/settings/staging.py
+++ caviciel/settings/staging.py
@@ -1,12 +1,16 @@
 from .base import *
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
 DATABASES = {
-    #    'default': {
-    #        'ENGINE': 'django.db.backends.sqlite3',
-    #        'NAME': BASE_DIR / 'db.sqlite3',
-    #    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'caviciel',
+            'USER': os.getenv('PG_USER'),
+            'PASSWORD': os.getenv('PG_PASSWORD'),
+            'HOST': os.getenv('PG_HOST'),
+            'PORT': os.getenv('PG_PORT')
+        }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
