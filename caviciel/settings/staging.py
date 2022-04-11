59fe62a6e96de5f36e3bfbd8b41cdce230ca840d
guishu <guillaume@schub.fr>
2022-04-11 17:40:55 +0200

    allow debugging as an option for staging

--- caviciel/settings/staging.py
+++ caviciel/settings/staging.py
@@ -19,3 +19,5 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
+
+DEBUG = os.getenv('DEBUG') == "True"
