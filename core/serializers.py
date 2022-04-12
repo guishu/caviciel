0cd87d4f69734e5bb7a719182112c79ef9094fa1
guishu <guillaume@schub.fr>
2022-04-12 11:25:39 +0200

    Producer list returns all data after all

--- core/serializers.py
+++ core/serializers.py
@@ -3,12 +3,6 @@ from rest_framework import serializers
 from core.models import Producer
 
 
-class ProducerListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Producer
-        fields = ["id"]
-
-
 class ProducerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producer
--- core/views.py
+++ core/views.py
@@ -2,17 +2,13 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import Producer
-from core.serializers import ProducerSerializer, ProducerListSerializer
+from core.serializers import ProducerSerializer
 
 
 class ProducerViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Producer.objects.all()
+    serializer_class = ProducerSerializer
 
-    def get_serializer_class(self):
-        if self.action == "list":
-            return ProducerListSerializer
-        else:
-            return ProducerSerializer
 
 
